90496895727059b75a6ff02140785b71eca7ab10
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-09 11:49:56 -0600

    Adding Client-Side Page Guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,9 +1,24 @@
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
 
 function UserProfile() {
   // Redirect away if NOT auth
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = '/auth';
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [getSession]);
 
+  if (isLoading) {
+    return <p className={classes.center}>Loading...</p>;
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
